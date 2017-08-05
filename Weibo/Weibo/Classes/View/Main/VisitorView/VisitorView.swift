a037972a9d42b4395ee7e90f8b58cc4e3df2f7ac
Cstzhang <835066041@qq.com>
2017-08-05 20:36:25 +0800

    添加UI适配

--- Weibo/Weibo/Classes/View/Main/VisitorView/VisitorView.swift
+++ Weibo/Weibo/Classes/View/Main/VisitorView/VisitorView.swift
@@ -22,31 +22,85 @@ class VisitorView: UIView {
 
    // MARK: - 私有控件
     //圈视图
-    private lazy var iconView:UIImageView = UIImageView(image: UIImage.init(named: "visitordiscover_feed_image_smallicon"))
+    fileprivate lazy var iconView:UIImageView = UIImageView(image: UIImage.init(named: "visitordiscover_feed_image_smallicon"))
     //房子
-    private lazy var houseIconView:UIImageView = UIImageView(image: UIImage.init(named: "visitordiscover_feed_image_house"))
+    fileprivate lazy var houseIconView:UIImageView = UIImageView(image: UIImage.init(named: "visitordiscover_feed_image_house"))
     //提示语
-    private lazy var tipLabel:UILabel = UILabel.cz_label(withText: "关注一些人,回这里看看有什么惊喜",
+    fileprivate lazy var tipLabel:UILabel = UILabel.cz_label(withText: "关注一些人,回这里看看有什么惊喜",
                                                       fontSize: 14,
                                                       color: UIColor.darkGray)
     //注册按钮
-    private lazy var registerButton:UIButton = UIButton.cz_textButton("注册", fontSize: 16,
-                                                                      normalColor: UIColor.orange,
-                                                                      highlightedColor: UIColor.black,
-                                                                      backgroundImageName: "common_button_white_disable")
+    fileprivate lazy var registerButton:UIButton = UIButton.cz_textButton(
+        "注册",
+        fontSize: 16,
+        normalColor: UIColor.orange,
+        highlightedColor: UIColor.black,
+        backgroundImageName: "common_button_white_disable")
     //登录按钮
-    private lazy var loginButton:UIButton = UIButton.cz_textButton("登录", fontSize: 16,
-                                                                   normalColor: UIColor.darkGray,
-                                                                   highlightedColor: UIColor.black,
-                                                                   backgroundImageName: "common_button_white_disable")
+    fileprivate lazy var loginButton:UIButton = UIButton.cz_textButton(
+        "登录",
+        fontSize: 16,
+        normalColor: UIColor.darkGray,
+        highlightedColor: UIColor.black,
+        backgroundImageName: "common_button_white_disable")
     
 }
 
 extension VisitorView{
-
     func setupUI()  {
-        backgroundColor = UIColor.red
+        backgroundColor = UIColor.white
+        addSubview(iconView)
+        addSubview(houseIconView)
+        addSubview(tipLabel)
+        addSubview(registerButton)
+        addSubview(loginButton)
+        //取消 autoresizing
+        for v in subviews {
+            v.translatesAutoresizingMaskIntoConstraints = false
+        }
+        //自动布局
+        //圆圈背景图视图
+        addConstraint(NSLayoutConstraint(item: iconView,
+                                       attribute: .centerX,
+                                       relatedBy: .equal,
+                                       toItem: self,
+                                       attribute: .centerX,
+                                       multiplier: 1.0,
+                                       constant: 0))
+        addConstraint(NSLayoutConstraint(item: iconView,
+                                         attribute: .centerY,
+                                         relatedBy: .equal,
+                                         toItem: self,
+                                         attribute: .centerY,
+                                         multiplier: 1.0,
+                                         constant: -60))
+    
+        //房子
+        addConstraint(NSLayoutConstraint(item: houseIconView,
+                                         attribute: .centerX,
+                                         relatedBy: .equal,
+                                         toItem: iconView,
+                                         attribute: .centerX,
+                                         multiplier: 1.0,
+                                         constant: 0))
+        addConstraint(NSLayoutConstraint(item: houseIconView,
+                                         attribute: .centerY,
+                                         relatedBy: .equal,
+                                         toItem: iconView,
+                                         attribute: .centerY,
+                                         multiplier: 1.0,
+                                         constant: 0))
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
+    
 
 
 }
