5d76d2547efadfd3a8894e346c4fd37e955fc124
Cstzhang <835066041@qq.com>
2017-10-17 16:14:45 +0800

    模拟网络数据加载

--- Weibo/Weibo/AppDelegate.swift
+++ Weibo/Weibo/AppDelegate.swift
@@ -21,9 +21,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.backgroundColor = UIColor.white
         window?.rootViewController = MainViewController()
         window?.makeKeyAndVisible()
+        loadAppInfo()
         return true
     }
 
 
 }
+
+// MARK: -从服务器加载应用程序信息
+extension AppDelegate {
+    func loadAppInfo() -> () {
+        DispatchQueue.global().async {
+            //1.url
+            let url = Bundle.main.url(forResource: "MainVC.json", withExtension: nil)
+            //2.读取json文件中的内容
+             let data = NSData(contentsOf: url!)
+            //3.写入沙盒磁盘
+            let docDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+            let jsonPath = (docDir as NSString).appendingPathComponent("MainVC.json")
+            data?.write(toFile: jsonPath, atomically: true)
+            print("应用程序加载完毕:\(jsonPath)")
+            
+            
+        }
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+}
+
 
