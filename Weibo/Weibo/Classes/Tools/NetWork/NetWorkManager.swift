646c4f551a369ca82bfb970cf2adcedabe2975cc
Cstzhang <835066041@qq.com>
2017-10-18 19:09:10 +0800

    屏蔽多余代码

--- Weibo/Weibo/Classes/Tools/NetWork/NetWorkManager.swift
+++ Weibo/Weibo/Classes/Tools/NetWork/NetWorkManager.swift
@@ -30,16 +30,16 @@ class NetWorkManager: AFHTTPSessionManager {
     ///   - completion: 完成回调[json(字典/数组)，是否成功]
     func request(method:HTTPMethod = .GET,URLString:String,parameters:[String:AnyObject],completion:@escaping (_ json:AnyObject?,_ isSuccess:Bool)->()) {
         print("准备网络请求")
-        //成功回调
-        let success   = {(task:URLSessionDataTask,json:AnyObject?)-> Void in
-            print("网络请求成功")
-            completion(json, true)
-        }
-        //失败回调
-        let failure = { (task:URLSessionDataTask?,error:NSError)-> Void in
-            print("网络请求错误\(error)")
-            completion(nil, false)
-        }
+//        //成功回调
+//        let success   = {(task:URLSessionDataTask,json:AnyObject?)-> Void in
+//            print("网络请求成功")
+//            completion(json, true)
+//        }
+//        //失败回调
+//        let failure = { (task:URLSessionDataTask?,error:NSError)-> Void in
+//            print("网络请求错误\(error)")
+//            completion(nil, false)
+//        }
         
         if method == .GET {
             get(URLString, parameters: parameters, progress: nil, success: { (_, json) in
