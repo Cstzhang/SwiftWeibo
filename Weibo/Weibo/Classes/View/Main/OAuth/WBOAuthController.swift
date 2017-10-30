76fa718a5d895cae7f6cf06ac7fdd9f71518e42e
Cstzhang <835066041@qq.com>
2017-10-30 16:40:47 +0800

    加载登录页

--- Weibo/Weibo/Classes/View/Main/OAuth/WBOAuthController.swift
+++ Weibo/Weibo/Classes/View/Main/OAuth/WBOAuthController.swift
@@ -18,11 +18,25 @@ class WBOAuthController: UIViewController {
         title = "登录新浪微博"
         //按钮
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "返回", fontSize: 14, target: self, action: #selector(loginClose), isBack: true)
+        
     }
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        //加载授权页面
+        let urlString = "https://api.weibo.com/oauth2/authorize?client_id=\(WBAppKey)&redirect_uri=\(WBRedirectURL)"
+        
+        guard  let url = URL(string: urlString) else{
+            return
+        }
+       //创建请求
+       let  request = URLRequest(url: url)
+       //加载请求
+       webView.loadRequest(request)
+        
+       
+        
 
 
     }
