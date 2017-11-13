f4e557769aedc9e4e0837dac1e6b2606df8fbb21
Cstzhang <835066041@qq.com>
2017-11-13 22:37:25 +0800

    下拉动画

--- Weibo/Weibo/Classes/Tools/ZBRefresh/ZBMtRefreshView.swift
+++ Weibo/Weibo/Classes/Tools/ZBRefresh/ZBMtRefreshView.swift
@@ -15,4 +15,26 @@ class ZBMtRefreshView: ZBRefreshView {
     @IBOutlet weak var earthIconView: UIImageView!
     
     @IBOutlet weak var kangarooIconView: UIImageView!
+    
+    /// 动画
+    override func awakeFromNib() {
+        //房子
+        let buildImage1 = #imageLiteral(resourceName: "icon_building_loading_1")
+        let buildImage2 = #imageLiteral(resourceName: "icon_building_loading_2")
+        buildingIconView.image = UIImage.animatedImage(with: [buildImage1,buildImage2], duration: 0.5)
+        //地球
+        let animation = CABasicAnimation(keyPath: "transform.rotation")
+        animation.toValue = -2 * Double.pi //负数是逆时针
+        animation.repeatCount = MAXFLOAT
+        animation.duration = 3
+        animation.isRemovedOnCompletion = false
+        earthIconView.layer.add(animation, forKey: nil)
+        //袋鼠
+        
+        
+        
+    }
+    
+    
+    
 }
