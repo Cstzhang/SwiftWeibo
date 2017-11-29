bfbea4fef71f64769d9b641927c4fb0750aa5fca
恒信永利 <hengxinyongli@zzbwork.local>
2017-11-29 14:21:47 +0800

    撰写微博的文本视图

---
+++ Weibo/Weibo/Classes/View/Compose/View/WBComposeTextView.swift
@@ -0,0 +1,15 @@
+//
+//  WBComposeTextView.swift
+//  Weibo
+//
+//  Created by 恒信永利 on 2017/11/29.
+//  Copyright © 2017年 zhangzb. All rights reserved.
+//  //撰写微博的文本视图
+
+import UIKit
+
+class WBComposeTextView: UITextView {
+
+  
+
+}
