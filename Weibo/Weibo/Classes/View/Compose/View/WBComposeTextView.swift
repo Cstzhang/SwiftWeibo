16e29daa22710063a459c812307cda4a14505b5b
恒信永利 <hengxinyongli@zzbwork.local>
2017-11-29 14:26:16 +0800

    添加占位Label

--- Weibo/Weibo/Classes/View/Compose/View/WBComposeTextView.swift
+++ Weibo/Weibo/Classes/View/Compose/View/WBComposeTextView.swift
@@ -4,12 +4,31 @@
 //
 //  Created by 恒信永利 on 2017/11/29.
 //  Copyright © 2017年 zhangzb. All rights reserved.
-//  //撰写微博的文本视图
+//
 
 import UIKit
-
+ //撰写微博的文本视图
 class WBComposeTextView: UITextView {
-
+    private lazy var placeholderlabel = UILabel()
+    override func awakeFromNib() {
+        setupUI()
+    }
   
 
+}
+
+private extension WBComposeTextView{
+   func setupUI(){
+     // 1 设置占位标签
+    placeholderlabel.text = "分享新鲜事..."
+    placeholderlabel.font = self.font
+    placeholderlabel.textColor = UIColor.lightGray
+    placeholderlabel.frame.origin = CGPoint(x: 5, y: 8)
+    placeholderlabel.sizeToFit()
+    
+    
+    
+    }
+    
+    
 }
