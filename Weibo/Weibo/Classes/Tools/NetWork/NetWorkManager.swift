1416ea5d9724a0292b6359044d384e099d941929
恒信永利 <hengxinyongli@zzbwork.local>
2017-11-29 19:27:29 +0800

    拓展网络请求方法

--- Weibo/Weibo/Classes/Tools/NetWork/NetWorkManager.swift
+++ Weibo/Weibo/Classes/Tools/NetWork/NetWorkManager.swift
@@ -66,7 +66,17 @@ class NetWorkManager: AFHTTPSessionManager {
     
     
     // 获取拼接  accessToken
-    func tokenRequest(method:HTTPMethod = .GET,URLString:String,parameters:[String:AnyObject]?,completion:@escaping (_ json:AnyObject?,_ isSuccess:Bool)->()) {
+    
+    /// 含有token的网络请求
+    ///
+    /// - Parameters:
+    ///   - method: get/post
+    ///   - URLString: url
+    ///   - parameters: 参数字典
+    ///   - name: 上传文件使用的字段名，默认为nil nil 则不是上传文件
+    ///   - data: 上传文件的数据，默认为nil nil则不是上传文件
+    ///   - completion: 完成回调
+    func tokenRequest(method:HTTPMethod = .GET,URLString:String,parameters:[String:AnyObject]?,name:String? = nil ,data:Data? = nil ,completion:@escaping (_ json:AnyObject?,_ isSuccess:Bool)->()) {
         //0,判断toke 是否为nil 程序执行过程中一般不会为nil
         guard let token = userAccount.access_token else {
             print("无token,需要登录！")
@@ -85,8 +95,17 @@ class NetWorkManager: AFHTTPSessionManager {
         }
         //2，设置字典参数 parameters一定有值
         parameters!["access_token"] = token as AnyObject
-        //3,请求
-        request(method: method, URLString: URLString, parameters: parameters, completion: completion)
+        
+        //3, 判断参数 请求
+        if let name = name, let data = data {
+            //上传文件
+            upload(url: URLString, parameters: parameters, name: name, data: data, completion: completion)
+            
+        }else{
+            //,普通请求
+            request(method: method, URLString: URLString, parameters: parameters, completion: completion)
+            
+        }
         
     }
     //
@@ -100,8 +119,17 @@ class NetWorkManager: AFHTTPSessionManager {
     ///   - data: 图片数据
     ///   - completion: 完成回调
     func upload(url:String,parameters:[String:AnyObject]?,name:String,data:Data,completion:@escaping (_ json:AnyObject?,_ isSuccess:Bool)->()) -> () {
-        post(url, parameters: parameters, constructingBodyWith: { (fromData) in
+        post(url, parameters: parameters, constructingBodyWith: { (formData) in
             //FIXME: -  创建 fromData
+            /*
+             1,data 要上传的二进制数据
+             2,name 服务器接收数据的字段名
+             3,fileName 保存在服务器的文件名，大多数服务器上传后生成缩略图，中图，大图
+             4，mimeType 上传文件的类型 可以 appliction/octet-stream
+                 "image/png image/jpg image/gif"
+             */
+            
+            formData.appendPart(withFileData: data, name: name, fileName: "xxx", mimeType: "application/octet-stream")
             
         }, progress: nil, success: { (_, json) in
             completion(json as AnyObject, true)
