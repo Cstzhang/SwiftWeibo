332f1d217594b6476e52354f6d10142e5f72a232
恒信永利 <hengxinyongli@zzbwork.local>
2017-11-30 15:27:17 +0800

    键盘按钮布局

--- Weibo/Weibo/Classes/Tools/Emoticon/WBEmoticonToolBar.swift
+++ Weibo/Weibo/Classes/Tools/Emoticon/WBEmoticonToolBar.swift
@@ -4,18 +4,57 @@
 //
 //  Created by 恒信永利 on 2017/11/30.
 //  Copyright © 2017年 zhangzb. All rights reserved.
-//
+//  表情键盘底部工具栏
 
 import UIKit
 
 class WBEmoticonToolBar: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        setupUI()
     }
-    */
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let count  = subviews.count
+        let w  = bounds.width / CGFloat(count)
+        let rect  = CGRect(x: 0, y: 0, width: w, height: bounds.height)
+        for (i ,btn ) in subviews.enumerated() {
+            btn.frame = rect.offsetBy(dx: CGFloat(i) * w, dy: 0)
+        }
+        
+        
+        
+    }
 
+}
+
+private extension WBEmoticonToolBar{
+    
+    func setupUI() -> () {
+      //设置按钮（读取表情包分组名称）
+      //0 获取表情管理器单例
+        let manager  = ZBEmoticonManager.shared
+      //1 获取分组名称 设置按钮
+        for p  in manager.packages {
+            let btn  = UIButton()
+            btn.setTitle(p.groupName, for: [])
+            btn.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+            btn.setTitleColor(UIColor.white, for: [])
+            btn.setTitleColor(UIColor.darkGray, for: .highlighted)
+            btn.setTitleColor(UIColor.darkGray, for: .selected)
+            btn.backgroundColor = UIColor.blue
+            // 3 添加按钮
+            btn.sizeToFit()
+            addSubview(btn)
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
 }
