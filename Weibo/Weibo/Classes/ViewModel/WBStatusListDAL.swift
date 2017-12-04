f06e5ad3161ecc8de704736a1a11dc8a9433d95a
恒信永利 <hengxinyongli@zzbwork.local>
2017-12-04 10:32:56 +0800

    新增数据访问层

---
+++ Weibo/Weibo/Classes/ViewModel/WBStatusListDAL.swift
@@ -0,0 +1,19 @@
+//
+//  WBStatusListDAL.swift
+//  Weibo
+//
+//  Created by 恒信永利 on 2017/12/4.
+//  Copyright © 2017年 zhangzb. All rights reserved.
+//
+
+import Foundation
+// DAL data access layer 数据访问层
+// 负责处理数据库和网络数据，给listViewModel 返回微博数据
+class WBStatusListDAL {
+    
+    
+    
+    
+    
+    
+}
