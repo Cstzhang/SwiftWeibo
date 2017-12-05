f9de252d0733f9469ce8b541e257dff6bc0c22ca
恒信永利 <hengxinyongli@zzbwork.local>
2017-12-05 11:05:23 +0800

    设置分页图片

--- Weibo/Weibo/Classes/Tools/Emoticon/WBEmoticonInputVIew.swift
+++ Weibo/Weibo/Classes/Tools/Emoticon/WBEmoticonInputVIew.swift
@@ -33,7 +33,18 @@ class WBEmoticonInputVIew: UIView {
         collectionView.register(ZBEmoticonCell.self, forCellWithReuseIdentifier: cellId)
         //设置代理
         toolBar.delegate = self
-        
+        //设置分页控件的图片
+        let bundle  = ZBEmoticonManager.shared.bundle
+        guard  let normalImage  = UIImage(named: "compose_keyboard_dot_normal", in: bundle, compatibleWith: nil),
+            let selectedImage  = UIImage(named: "compose_keyboard_dot_selected", in: bundle, compatibleWith: nil) else{
+                return
+        }
+        //使用填充图片设置颜色
+        //        pageControl.pageIndicatorTintColor = UIColor(patternImage: normalImage)
+        //        pageControl.currentPageIndicatorTintColor = UIColor(patternImage: selectedImage)
+        // 使用KVC 设置私有成员属性
+        pageControl.setValue(normalImage, forKey: "_pageImage")
+        pageControl.setValue(selectedImage, forKey: "_currentPageImage")
     }
 
 }
@@ -74,6 +85,10 @@ extension WBEmoticonInputVIew:UICollectionViewDelegate{
         }
         //显示对应的分组
         toolBar.selectedIndex = target.section
+        //设置分页控件
+        pageControl.numberOfPages = collectionView.numberOfItems(inSection: target.section)
+        pageControl.currentPage = target.item
+
     }
     
     
